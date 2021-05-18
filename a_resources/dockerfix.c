cc7eb4d7394d74aeef2be5f4a1c4ffa88395fb29
Niks Hariman <nhariman@hotmail.com>
2021-05-18 19:51:01 +0200

    replaced certs with .docker certs to see if that helps the ssl problem

--- a_resources/dockerfix.c
+++ a_resources/dockerfix.c
@@ -1,3 +1,12 @@
 mkdir -p ~/goinfre/docker
 rm -rf ~/Library/Containers/com.docker.docker
 ln -s ~/goinfre/docker ~/Library/Containers/com.docker.docker
+
+#stop all running containers:
+docker stop $(docker ps -aq)
+
+#remove all containers
+docker rm $(docker ps -aq)
+
+#remove all images:
+docker rmi $(docker images -q)
