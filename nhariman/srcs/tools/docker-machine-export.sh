299121e3ada8fea532bb810b9b9d8f6ebb932673
Niks Hariman <nhariman@hotmail.com>
2021-05-22 15:10:45 +0200

    updated a bunch of stuff but it probably doesn't work anymore lmao

---
+++ nhariman/srcs/tools/docker-machine-export.sh
@@ -0,0 +1,18 @@
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.142:2376"
+export DOCKER_CERT_PATH="/Users/nhariman/.docker/machine/machines/inception"
+export DOCKER_MACHINE_NAME="inception"
+# Run this command to configure your shell: 
+# eval $(docker-machine env inception)
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.143:2376"
+export DOCKER_CERT_PATH="/Users/nhariman/.docker/machine/machines/inception"
+export DOCKER_MACHINE_NAME="inception"
+# Run this command to configure your shell: 
+# eval $(docker-machine env inception)
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.144:2376"
+export DOCKER_CERT_PATH="/Users/nhariman/.docker/machine/machines/inception"
+export DOCKER_MACHINE_NAME="inception"
+# Run this command to configure your shell: 
+# eval $(docker-machine env inception)
